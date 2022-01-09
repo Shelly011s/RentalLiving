e802a6df97640a38a6ccb5e1d2cfd1b001a3b019
Shelly Suthar <84381373+Shelly011s@users.noreply.github.com>
2022-01-09 22:50:52 +0530

    Update app.js

--- RentalLiving/app.js
+++ RentalLiving/app.js
@@ -58,6 +58,6 @@ app.use("/houses", houseRoutes);
 app.use("/houses/:id/comments", commentRoutes);
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is up and running');
   });
